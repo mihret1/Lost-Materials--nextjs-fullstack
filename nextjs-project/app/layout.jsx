6fec07beab275fc84d29f8cacc76b20c9861d501
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-07-21 15:53:44 +0300

    add some file

---
+++ nextjs-project/app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css'
+
+
+export const metadata={
+    title:'prompotia',
+    description:'discover and share your memories'
+}
+
+const Layout=()=>{
+    return(
+        <html lang='en'>
+            <body>
+               <div className='main '>
+                <div />
+               </div>
+            </body>
+        </html>
+    )
+}
+
+
+export default Layout

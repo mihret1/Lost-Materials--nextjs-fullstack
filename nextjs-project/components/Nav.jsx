d96aa34c709b8aded63f613958b5723d9d845433
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-10-22 14:31:56 +0300

    add desktp and mobile responsiveness on navbar

--- nextjs-project/components/Nav.jsx
+++ nextjs-project/components/Nav.jsx
@@ -8,6 +8,18 @@ import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
 
 const Nav=()=>{
     const isUserLoggedIn=true
+    const [providers,setProviders]=useState(null)
+    const [toggleDropdown,setToggeleDropdown]=useState(true)
+
+    useEffect(()=>{
+        const setProviders=async ()=>{
+            const response=await getProviders();
+            setProviders(response)
+        }
+        setProviders()
+    },[])
+
+    
     return(
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href='/' className="flex  gap-2 flex-center">
@@ -19,9 +31,9 @@ const Nav=()=>{
                      className="object-contain"
                  />
                  <p className="logo_text">LostMaterials</p>
-                  {/*desktop nav*/}
-                
              </Link>
+             
+        {/*desktop nav*/}                
              <div className="sm:flex hidden">
                      {isUserLoggedIn ?(
                         <div className="flex gap-3 md:gap-5">
@@ -41,7 +53,81 @@ const Nav=()=>{
                                  />
                             </Link>
                         </div>
-                     ):(<></>)}
+                     ):(
+                     <>
+                       {providers && 
+                              Object.values(providers).map((provider)=>(
+                                <button 
+                                    type="button" 
+                                    key={provider.name}
+                                    onClick={()=>signIn(provider.id)}
+                                    className="black_btn"
+                                    >
+                                    Sign In
+                                </button>
+                              ))
+                       }
+                     </>
+                     )}
+                  </div>
+
+                  {/* mobile navigation*/}
+                  <div className="sm:hidden flex relative"> 
+                    {isUserLoggedIn ?(
+                        <div className="flex">
+                                <Image 
+                                   src='/assets/images/logo.svg'
+                                   width={37}
+                                   height={37}
+                                   className="rounded-full"
+                                   alt='profile'
+                                   onClick={()=>{setToggeleDropdown((prev)=>!prev)}}
+                                 />
+                                 {toggleDropdown && (
+                                    <div className="dropdown">
+                                        <Link 
+                                           href='/profile'
+                                           className="dropdown_link"
+                                           onClick={()=>setToggeleDropdown(false)}>
+                                            My Profile
+                                         </Link>
+                                         <Link 
+                                           href='/create-prompt'
+                                           className="dropdown_link"
+                                           onClick={()=>setToggeleDropdown(false)}>
+                                            Create Prompt
+                                           </Link>
+                                           <button
+                                                type='button'
+                                                onClick={()=>{
+                                                    setToggeleDropdown(false)
+                                                    signOut()
+                                                }}
+                                                className="mt-5 w-full black_btn">
+                                                    Sign Out
+
+                                           </button>
+                                    </div>
+                                 )}
+                        </div>
+
+
+                    ):(
+                        <>
+                        {providers && 
+                               Object.values(providers).map((provider)=>(
+                                 <button 
+                                     type="button" 
+                                     key={provider.name}
+                                     onClick={()=>signIn(provider.id)}
+                                     className="black_btn"
+                                     >
+                                     Sign In
+                                 </button>
+                               ))
+                        }
+                      </>
+                    )}
                   </div>
              
         </nav>

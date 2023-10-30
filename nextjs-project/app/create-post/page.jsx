71146682f35f40b2035ada4c9178ea77c80953d2
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-10-30 23:22:04 +0300

    start create post page

---
+++ nextjs-project/app/create-post/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreatePost=()=>{
+    return(
+        <div>
+            create posts here
+        </div>
+    )
+}
+
+
+export default CreatePost
--- nextjs-project/components/Nav.jsx
+++ nextjs-project/components/Nav.jsx
@@ -36,7 +36,7 @@ const Nav=()=>{
              <div className="sm:flex hidden">
                      {session?.user ?(
                         <div className="flex gap-3 md:gap-5">
-                            <Link href='/create-prompt' className="black_btn">
+                            <Link href='/create-post' className="black_btn">
                                 create post
                             </Link>
                             <button type='button' onClick={signOut} className="outline_btn">

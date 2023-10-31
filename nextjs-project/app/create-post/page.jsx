e5fe3644eb7fd113f709267ef927bf776d6d0aaa
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-10-31 11:44:10 +0300

    finish signIn

--- nextjs-project/app/create-post/page.jsx
+++ nextjs-project/app/create-post/page.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client'
+
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import Form from "@components/Form"
+
 
 const CreatePost=()=>{
+    const [submitting,setSubmiting]=useState(false)
+    const [post,setPost]=useState({
+        prompt:'',
+        tag:''
+    })
+
+    const createPost=async(e)=>{}
+
     return(
         <div>
-            create posts here
+            <Form 
+                type="Create"
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={createPost}
+            
+            />
         </div>
     )
 }
--- nextjs-project/components/Form.jsx
+++ nextjs-project/components/Form.jsx
@@ -1,8 +1,37 @@
-import React from "react"
+import Link from "next/link"
 
-const Form=()=>{
+const Form=({type,post,setPost,submitting,handleSubmit})=>{
     return(
-        <div>form</div>
+        <section className="w-full  max-w-full flex-start flex-col">
+            <h2 className="head_text text-left">
+                <span className="blue_gradient">{type} Posts</span>
+            </h2>
+            <p className="desc text-left max-w-md">
+                {type} and share what you get on lost which is not your and make happy other by returning it to the owner  and share this kidness to whole the world
+            </p>
+            <form 
+               onSubmit={handleSubmit}
+               className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+            >
+             <label>
+                <span className="font-satoshi font-semibold text-base text-gray-700">
+                    Material Found
+                </span>
+                <textarea 
+                   value={post.prompt}
+                   onChange={(e)=>setPost({...post,prompt:e.target.value})}
+                   placeholder="write what you found"
+                   required
+                   className="form_textarea"
+                
+                />
+             </label>
+
+            </form>
+            
+            
+            
+            </section>
     )
 }
 

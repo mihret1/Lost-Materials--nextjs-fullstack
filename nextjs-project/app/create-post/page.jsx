6a215871de4f1daa339fe91548b7759c535ff011
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-10-31 13:38:53 +0300

    done create post functionality

--- nextjs-project/app/create-post/page.jsx
+++ nextjs-project/app/create-post/page.jsx
@@ -13,7 +13,31 @@ const CreatePost=()=>{
         tag:''
     })
 
-    const createPost=async(e)=>{}
+    const createPost=async(e)=>{
+          e.preventDefault()
+          setSubmiting(true)
+
+          try{
+            const response=await fetch('/api/prompt/new',
+            {
+                method:'POST',
+                body:JSON.stringify({
+                    prompt:post.prompt,
+                    tag:post.tag,
+                    userId:session?.user.id
+                })
+            })
+            if(response.ok){
+                router.push('/')
+            }
+
+          }catch(error){
+            console.log(error)
+
+          }finally{
+            setSubmiting(false)
+          }
+    }
 
     return(
         <div>
